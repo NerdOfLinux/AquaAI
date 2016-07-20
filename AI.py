da28428cd22ae7a0cfff5d04e224476d40541a4c
NerdOfLinux <golf@runningwind.com>
2016-07-19 23:15:47 -0500

    Update AI.py

--- AI.py
+++ AI.py
@@ -11,11 +11,14 @@ if filecheck == 1:
 else:
 	print("Welcome to the AquaAI setup.")
 	username=raw_input("What is your name?: ")
+	while " " or "-" in username:
+		print("No spaces or special characters allowed!!!")
+		username=raw_input("What is your name?: ")
 	print("Hello, %s, please answer a few questions about yourself:" %username)
 	color=raw_input("What is your favorite color?: ")
 	birthday=raw_input("When is your birthday (month/day in numbers)?: ")
 	with open('AI.dat', 'w') as file:
 		file.write("%s" %username)
-	with open('%s.pl', 'w') as file:
+	with open('%s.pl', 'w' %username) as file:
 		file.write("favoritecolor(%s)." %color)
 		file.write("birthday(%s)." %birthday)
