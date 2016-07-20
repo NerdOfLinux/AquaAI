233a9b5133417389416d7dbe7a2b0fcf30bcba8e
NerdOfLinux <golf@runningwind.com>
2016-07-20 09:03:59 -0500

    Update AI.py

--- AI.py
+++ AI.py
@@ -1,7 +1,7 @@
 import sys
 import os
 #filecheck=os.path.isfile("AI.pl")
-filecheck=0
+filecheck=os.path.isfile("AI.dat")
 if filecheck == 1:
 	file=open('AI.dat', 'r')
 	username=raw_input("What is your username?: ")
