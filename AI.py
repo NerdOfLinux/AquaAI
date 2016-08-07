6207188dfedad06928b01ca6e1cce77ecce525c7
NerdOfCode <Lovelaptops18@gmail.com>
2016-08-07 18:58:46 -0400

    Update AI.py
    fixed an error line 135 fav number

--- AI.py
+++ AI.py
@@ -132,5 +132,5 @@ else:
 	#Generate AI favorites
 	with open ('AI.dat', 'w') as file:
 		file.write("my favorite color is %s. \n" %random.choice(colors))
-		file.write("my favorite number is %s. \n" %random.choice(0,999))
+		file.write("my favorite number is %s. \n" %random.randint(0,999))
 		
