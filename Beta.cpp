1645441832d198a0867f91061457fa59be11e50e
NerdOfLinux <golf@runningwind.com>
2016-08-07 13:18:22 -0500

    Updated Beta.coo

--- Beta.cpp
+++ Beta.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main()
 {
-   cout << "Welcome to AquaAI";
-   return 0;
+	//Define username as a string
+	string username;
+	//Print Welcome to AquaAI
+	cout << "Welcome to AquaAI." << endl;
+	//Get username
+	cout << "Please enter your username: ";
+	getline (cin, username);
+	//Print Welcome
+	cout << "Welcome, " << username << "." << endl;
+	//exit
+	return 0;
 }
