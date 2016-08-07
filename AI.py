19d03d5d9a813d4667f6296a9a6779e64bdafdb3
NerdOfCode <Lovelaptops18@gmail.com>
2016-08-07 18:45:10 -0400

    Update
    Added fav number to AI

--- AI.py
+++ AI.py
@@ -119,4 +119,5 @@ else:
 	colors = ['yellow', 'red', 'blue', 'purple', 'orange']
 	with open ('AI.dat', 'w') as file:
 		file.write("my favorite color is %s. \n" %random.choice(colors))
-		file.write("my favorite number is %i." %random.randint(0, 999))
+	with open ('AI.dat', 'w') as file:
+		file.write("my favorite number is %s. \n" %random.choice(0,100))
