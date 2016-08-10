f379f362216b8413db07e743d2f3b2b76171ac4f
NerdOfLinux <golf@runningwind.com>
2016-08-09 19:12:29 -0500

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,6 +1,10 @@
-
-git clone https://github.com/simpleai-team/simpleai.git
-cd simpleai
+if [ $1 = "wget" ]
+then
+  wget https://pypi.python.org/packages/16/5e/62ccc6a4a154e0822f02b3918fb4c666130bf54b2fa4e0a7fa43057c3bf2/SpeechRecognition-3.4.6.tar.gz
+elif [ $1 = "curl" ]
+then
+  curl -OL https://pypi.python.org/packages/16/5e/62ccc6a4a154e0822f02b3918fb4c666130bf54b2fa4e0a7fa43057c3bf2/SpeechRecognition-3.4.6.tar.gz
+fi
+tar -zxf  SpeechRecognition-3.4.6.tar.gz
+cd SpeechRecognition-3.4.6
 python setup.py install --user
-cd ..
-rm -rf simpleai
