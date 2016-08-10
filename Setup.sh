bd8ca99b44b4cef7e9430987f2fbe5a946bca32b
NerdOfLinux <golf@runningwind.com>
2016-08-09 19:16:01 -0500

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -5,6 +5,6 @@ elif [ $1 = "curl" ]
 then
   curl -OL https://pypi.python.org/packages/16/5e/62ccc6a4a154e0822f02b3918fb4c666130bf54b2fa4e0a7fa43057c3bf2/SpeechRecognition-3.4.6.tar.gz
 fi
-tar -zxf  SpeechRecognition-3.4.6.tar.gz
+tar -zxvf  SpeechRecognition-3.4.6.tar.gz
 cd SpeechRecognition-3.4.6
 python setup.py install --user
