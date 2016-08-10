0cad0eb3044e67db328e3fa304c13d5a15e11abe
NerdOfLinux <golf@runningwind.com>
2016-08-09 19:19:37 -0500

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,10 +1,16 @@
 if [ $1 = "wget" ]
 then
   wget https://pypi.python.org/packages/16/5e/62ccc6a4a154e0822f02b3918fb4c666130bf54b2fa4e0a7fa43057c3bf2/SpeechRecognition-3.4.6.tar.gz
+  wget http://freenet.mcnabhosting.com/python/pyPortAudio/pyPortAudio.tar.gz
 elif [ $1 = "curl" ]
 then
   curl -OL https://pypi.python.org/packages/16/5e/62ccc6a4a154e0822f02b3918fb4c666130bf54b2fa4e0a7fa43057c3bf2/SpeechRecognition-3.4.6.tar.gz
+  curl -OL http://freenet.mcnabhosting.com/python/pyPortAudio/pyPortAudio.tar.gz
 fi
 tar -zxvf  SpeechRecognition-3.4.6.tar.gz
+tar -zxvf pyPortAudio.tar.gz
 cd SpeechRecognition-3.4.6
+python setup.py install --user
+cd ..
+cd pyPortAudio
 python setup.py install --user
