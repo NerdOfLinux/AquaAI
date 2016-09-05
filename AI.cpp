655e43461ad3ccdf0695adb46c16bb37c9e0a766
NerdOfLinux <golf@runningwind.com>
2016-09-05 12:14:54 -0500

    Update AI.cpp

--- AI.cpp
+++ AI.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 int main() {
   string name;
-  cout << "Hello, Welcome to Aqua-AI, Enter name now: \n";
-  getline(cin, name);
+  cout << "Hello, Welcome to Aqua-AI, Enter name now: ";
+  cin >> name;
+  cout << endl;
   
 }
