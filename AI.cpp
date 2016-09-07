23bdb52c31840cf28c66aa836502ea8f1e71fd0a
NerdOfLinux <golf@runningwind.com>
2016-09-07 17:37:02 -0500

    Update AI.cpp

--- AI.cpp
+++ AI.cpp
@@ -4,18 +4,19 @@
 #include <fstream>
 using namespace std;
   int main() {
-  createUser()
-}
-
-  int createUser(){
   string name;
   ofstream file;
   cout << "Hello, Welcome to Aqua-AI, Enter name now: ";
   getline(cin, name);
-  cout << "Welcome, " << name << "." << endl;
+  output=createUser(name)
+  cout << output << endl;
+}
+
+  int createUser(string name){
   file.open("AI.dat");
   file << name;
   file << "\n";
   file.close();
+  cout << "Welcome, " << name << "." << endl;
   return 0;
   }
