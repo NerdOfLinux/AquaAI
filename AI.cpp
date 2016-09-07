f0cc5c22d922a6add036e5c1a991963b27cac45d
NerdOfLinux <golf@runningwind.com>
2016-09-07 17:51:00 -0500

    Update AI.cpp

--- AI.cpp
+++ AI.cpp
@@ -3,20 +3,69 @@
 #include <string>
 #include <fstream>
 using namespace std;
+//Declare funtions
+int isFile(string isfile);
 int createUser(string name);
+int addUser(string name)
+//Main function
   int main() {
+  bool isFile;
+  //Make name a string
   string name;
+  //Get name from user
   cout << "Hello, Welcome to Aqua-AI, Enter name now: ";
   getline(cin, name);
-  createUser(name);
+  //If the file exists then add a user, else make a new file
+  isFile=isFile("AI.dat")
+  if (isFile == True){
+    addUser(name)
+  }
+  else {
+    createUser(name)
+  }
 }
-
+//Funtion createUser
   int createUser(string name){
+  //Declare what file is
   ofstream file;
+  //Make a file
   file.open("AI.dat");
+  //Put the name in the file
   file << name;
+  //Add a newline
   file << "\n";
+  //Close the file
   file.close();
   cout << "Welcome, " << name << "." << endl;
+  //End
+  return 0;
+  }
+//Function isFile
+  int isFile(string isfile) {
+    //If the file exists return 1
+    if (ifstream(isfile)) {
+     return True;
+    }
+    //Else return 0
+    else {
+      return False;
+    }
+    
+  }
+  
+//Funtion createUser
+  int addUser(string name){
+  //Declare what file is
+  ofstream file;
+  //Make a file
+  file.open("AI.dat", ios_base::app);
+  //Put the name in the file
+  file << name;
+  //Add a newline
+  file << "\n";
+  //Close the file
+  file.close();
+  cout << "Welcome, " << name << "." << endl;
+  //End
   return 0;
   }
