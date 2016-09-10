57ac81cd52b0cf22c52ac56ca8467ce1bacdbed7
NerdOfLinux <golf@runningwind.com>
2016-09-10 18:53:46 -0500

    Update AI.cpp

--- AI.cpp
+++ AI.cpp
@@ -6,7 +6,7 @@ using namespace std;
 //Declare funtions
 int createUser(string name);
 int addUser(string name);
-bool userFile(const char *filename);
+bool fexists(const char *filename);
 //Main function
   int main() {
   //int isFile; unused variable
@@ -77,7 +77,7 @@ bool userFile(const char *filename);
   }
 
 //Check if file exists
-bool userFile(const char *filename) {
+bool fexists(const char *filename) {
   ifstream ifile(filename);
   return ifile;
 }
