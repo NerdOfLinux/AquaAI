0b4cc989668e92bb18b301b95ae90bda479e1bd1
NerdOfLinux <golf@runningwind.com>
2016-09-11 15:49:17 -0500

    Update AI.cpp

--- AI.cpp
+++ AI.cpp
@@ -17,8 +17,9 @@ bool fexists(const char *filename);
   cout << "Hello, Welcome to Aqua-AI, Enter name now: ";
   getline(cin, name);
   //If the file exists then add a user, else make a new file with the user
-   if (fexists("users.dat")){
-      addUser(name);
+  userfile=name;
+   if (fexists(userfile.append(".dat").c_str())){
+      cout << "Welcome back, " << name << "." << endl;
     }
     else {
       createUser(name);
@@ -28,47 +29,17 @@ bool fexists(const char *filename);
   int createUser(string name){
   //Declare what file is
   string namefile;
-  ofstream file;
   ofstream userfile;
   //Make a file
-  file.open("users.dat");
   namefile=name;
   userfile.open(namefile.append(".dat").c_str());
-  //Make a file
-  file.open("users.dat");
-  //Put the name in the file
-  file << name;
-  //Add a newline
-  file << "\n";
   //Close the file
-  file.close();
   userfile.close();
   cout << "Welcome, " << name << "." << endl;
   //End
   return 0;
   }
   
-//Funtion addUser
-  int addUser(string name){
-  //Declare what file is
-  ofstream file;
-  ofstream userfile;
-  string namefile;
-  //Make a file
-  namefile=name;
-  file.open("users.dat", ios_base::app);
-  userfile.open(namefile.append(".dat").c_str());
-  file.open("users.dat", ios_base::app);
-  //Put the name in the file
-  file << name;
-  //Add a newline
-  file << "\n";
-  //Close the file
-  file.close();
-  userfile.close();
-  cout << "Welcome, " << name << "." << endl;
-  return 0;
-  }
 
 //Check if file exists
 bool fexists(const char *filename) {
