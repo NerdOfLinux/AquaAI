cb815c80cf0892186554123e23421a58f5290414
NerdOfCode <NerdOfCode@users.noreply.github.com>
2016-10-16 19:04:22 -0400

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -6,7 +6,7 @@ int main()
 {
   string search;
   cout << "Would you like to search that on your browser?" << endl;
-  getline(cin, search)
+  getline(cin, search);
   if ( search == "yes" ) {
     system("echo What would you like to search ; read search ; search2=${search// /%20}; x-www-browser https://www.google.com/#q=$search2");
   } else {
