2fdf85ae6bd63c56e4515172ab4b722a42016409
NerdOfLinux <golf@runningwind.com>
2016-10-16 17:51:31 -0500

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -8,7 +8,7 @@ cout << "Would you like to search that on your browser?" << endl;
 getline(cin, search);
 
 if ( search == "yes" ) {
-system("echo What would you like to search ; read search ; x-www-browser $search");
+system("echo What would you like to search ; read search ; x-www-browser https://www.google.com/#q=$search");
 } else {
 cout << "goodbye" << endl;
 }
