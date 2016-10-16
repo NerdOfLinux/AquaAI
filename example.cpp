a6791eff85f32b64cf1ef3a2c632d5f734f66d35
NerdOfCode <NerdOfCode@users.noreply.github.com>
2016-10-16 19:02:00 -0400

    Update example.cpp
    Removed nonfunctional c++ code

--- example.cpp
+++ example.cpp
@@ -6,8 +6,6 @@ int main()
 {
   string search;
   cout << "Would you like to search that on your browser?" << endl;
-  getline(cin, search);
-  replace(search.begin(), search.end(), ' ', '%20');
   if ( search == "yes" ) {
     system("echo What would you like to search ; read search ; x-www-browser https://www.google.com/#q=$search");
   } else {
