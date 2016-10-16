564a8e751d41199ec160a769ea45166b5e9571fd
NerdOfLinux <golf@runningwind.com>
2016-10-16 17:59:10 -0500

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -7,7 +7,7 @@ int main()
   string search;
   cout << "Would you like to search that on your browser?" << endl;
   getline(cin, search);
-  replace(string.begin(), string.end(), ' ', '%20');
+  replace(search.begin(), search.end(), ' ', '%20');
   if ( search == "yes" ) {
     system("echo What would you like to search ; read search ; x-www-browser https://www.google.com/#q=$search");
   } else {
