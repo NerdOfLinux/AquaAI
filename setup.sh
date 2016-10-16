e01e6917d082a9aeff236bc9eb16322cae3a7ab7
NerdOfLinux <golf@runningwind.com>
2016-10-16 14:20:13 -0500

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
-sudo apt install espeak
-clear
-make ; ./AI
+if [ $(which espeak) ]
+then
+	clear
+	make ; ./AI
+else
+	if [ $(whoami) = "root" ]
+	then
+		sudo apt install espeak
+	else
+		echo "Please run as root"
+		echo "Or manually install espeak."
+		exit 0
+	fi
+fi
