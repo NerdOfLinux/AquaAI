d47f38c1356031994e22e3ab253d80399248efd0
NerdOfLinux <golf@runningwind.com>
2016-10-16 17:56:57 -0500

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 int main()
 {
-string search;
-cout << "Would you like to search that on your browser?" << endl;
-getline(cin, search);
-
-if ( search == "yes" ) {
-system("echo What would you like to search ; read search ; x-www-browser https://www.google.com/#q=$search");
-} else {
-cout << "goodbye" << endl;
-}
+  string search;
+  cout << "Would you like to search that on your browser?" << endl;
+  getline(cin, search);
+  replace(string.begin(), string.end(), ' ', '%20');
+  if ( search == "yes" ) {
+    system("echo What would you like to search ; read search ; x-www-browser https://www.google.com/#q=$search");
+  } else {
+    cout << "goodbye" << endl;
+  }
 
 
 
