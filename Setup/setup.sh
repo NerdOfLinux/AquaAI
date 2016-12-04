184262fd61d18b573748fd892f8958e12cb55e7c
NerdOfLinux <golf@runningwind.com>
2016-12-04 16:23:15 -0600

    Update setup.sh

--- Setup/setup.sh
+++ Setup/setup.sh
@@ -1,34 +1,3 @@
 #!/bin/bash
-
-if [ $(which espeak) ]
-then
-	if [ $(which prolog) ]
-	then
-		cd ../Aqua-cpp
-		make ; ./Aqua
-	else
-		if [ $(whoami) = "root" ]
-		then
-			sudo apt-add-repository ppa:swi-prolog/stable
-			sudo apt-get update
-               		sudo apt-get install swi-prolog
-			sudo apt install espeak
-		else
-			echo "Please run as root"
-			echo "Or manually install espeak and swi prolog."
-			exit 0
-		fi
-	fi
-else
-	if [ $(whoami) = "root" ]
-	then
-		sudo apt-add-repository ppa:swi-prolog/stable
-		sudo apt-get update
-                sudo apt-get install swi-prolog
-		sudo apt install espeak
-	else
-		echo "Please run as root"
-		echo "Or manually install espeak and swi prolog."
-		exit 0
-	fi
-fi
+cd Aqua-cpp
+make
